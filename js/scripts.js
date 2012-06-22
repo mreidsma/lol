be56dccb716a2e0f512c7dcb2c07d83b94e6e551
Matthew Reidsma <matthew@reidsrow.com>
2012-06-22 00:11:50 -0400

    How did we go from fighting IE's CSS rendering to fighting Firefox? Squashed a few stupid FF annoyances.

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    
+    $("form.lib-form").find("label").hide(); // Hide labels if JS is available, since placeholders will show labels for form elements
+
+    if(!Modernizr.input.placeholder){ // Add placeholder text if HTML5 support broken
+
+        $('[placeholder]').focus(function() {
+          var input = $(this);
+          if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+          }
+        }).blur(function() {
+          var input = $(this);
+          if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+          }
+        }).blur();
+        $('[placeholder]').parents('form').submit(function() {
+          $(this).find('[placeholder]').each(function() {
+            var input = $(this);
+            if (input.val() == input.attr('placeholder')) {
+              input.val('');
+            }
+          })
+        });
+
+    }
+
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (viewportmeta) {
+    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+    document.body.addEventListener('gesturestart', function() {
+      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    }, false);
+  }
+}
+});
+
+
+
--- references.markdown
+++ references.markdown
@@ -4,8 +4,16 @@
 
 * Terms Library Users Understand: http://escholarship.org/uc/item/3qq499w7
 
+* Keith, Jeremy. (2011). Orientation and Scale. http://adactio.com/journal/4470/
+
+* Reidsma, Matthew. (2012). Control Issues. http://matthewreidsma.com/articles/19
+
+* Reidsma, Matthew. (2012). Libraries and the Myth of Mobile Phone Use. http://matthewreidsma.com/articles/21
+
 ## Tools and Scripts
 
 * Re-tabulate - keep navigation at the end of the source order while displaying it up top in wider screens using CSS 2.1: http://adactio.com/journal/4780/
 
 * Responsive Images
+
+* Test the Load Time of your page: http://tools.pingdom.com/fpt/
